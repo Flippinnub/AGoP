0deae70c28a9b62b7ad0ecfacab1b590a939b90f
Conner [REDACTED] <87244927+Flippinnub@users.noreply.github.com>
2022-02-23 07:44:47 -0600

    Update Setup.js
    add lockers

--- Setup.js
+++ Setup.js
@@ -8,6 +8,6 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 const scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0, 0, 0);
+camera.position.set(0, 4.166, -5);
 camera.rotation.set(-(Math.PI), 0, -(Math.PI));
 scene.add(camera);
