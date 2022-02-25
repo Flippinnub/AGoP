8c6b968a00eff8dc5d359b0c96607112588c03dc
Conner [REDACTED] <87244927+Flippinnub@users.noreply.github.com>
2022-02-25 07:07:22 -0600

    revert added controls

--- Animate.js
+++ Animate.js
@@ -1,9 +1,8 @@
 //Animate
 function animate() {
 	requestAnimationFrame(animate);
-    if (controls.enabled != true) {camera.position.z += 0.5;}
-    controls.update();
-    renderer.render(scene, camera);
+	camera.position.z += 0.1;
+	renderer.render(scene, camera);
 };
 
 animate();
