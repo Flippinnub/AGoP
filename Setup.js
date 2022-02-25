399f226103413165012e179d19b96c3f41ebd884
Conner [REDACTED] <87244927+Flippinnub@users.noreply.github.com>
2022-02-25 07:08:20 -0600

    revert added controls

--- Setup.js
+++ Setup.js
@@ -9,13 +9,5 @@ const scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 5, 0);
-controls.update();
 camera.rotation.set(180 * (Math.PI/180), 0, -(Math.PI));
 scene.add(camera);
-
-const controls = new FirstPersonControls(camera, renderer.domElement, scene);
-controls.enabled = true;
-
-controls.addEventListener( 'change', function () {
-    renderer.render( scene, camera );
-} );
