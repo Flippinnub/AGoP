8a28e60ad079c7ec7f3d8349ddb18bfee067284b
Conner [REDACTED] <87244927+Flippinnub@users.noreply.github.com>
2022-02-25 07:10:06 -0600

    fixed bug

--- Animate.js
+++ Animate.js
@@ -1,9 +1,7 @@
 //Animate
 function animate() {
-	requestAnimationFrame(animate);
-    
+    requestAnimationFrame(animate);
     camera.position.z += 0.5;
-
     renderer.render(scene, camera);
 };
 
