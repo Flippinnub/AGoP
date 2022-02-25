9f6442fc1d195e6e8b8b6e1604454ddf5a0b219c
Conner [REDACTED] <87244927+Flippinnub@users.noreply.github.com>
2022-02-24 22:39:04 -0600

    fixed another camera bug?

--- Animate.js
+++ Animate.js
@@ -1,7 +1,7 @@
 //Animate
 function animate() {
 	requestAnimationFrame(animate);
-    if (controls.enabled == false) {camera.position.z += 0.5;}
+    if (controls.enabled === false) {camera.position.z += 0.5;}
     controls.update();
     renderer.render(scene, camera);
 };
